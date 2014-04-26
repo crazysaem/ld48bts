a10ad6cb7b05b90e622d3d8fff88889dcee2e0c9
crazysaem <crazysaem@gmail.com>
2014-04-26 11:07:40 +0200

    initial commit

---
+++ html/src/com/crazysaem/bts/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.crazysaem.bts.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.crazysaem.bts.BTSGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new BTSGame();
+        }
+}
