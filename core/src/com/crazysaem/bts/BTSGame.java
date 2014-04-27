cfd6a82c8d001bae7f8370e312e6b31cd1938a38
crazysaem <crazysaem@gmail.com>
2014-04-27 14:53:28 +0200

    coins!

--- core/src/com/crazysaem/bts/BTSGame.java
+++ core/src/com/crazysaem/bts/BTSGame.java
@@ -4,24 +4,73 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BTSGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+public class BTSGame extends ApplicationAdapter
+{
+  private static final int COIN_NUMBER = 30;
+  private static final float COIN_SPEED = 350f;
+  private static final int SCREEN_HEIGHT = 800;
+  private static final int SCREEN_WIDTH = 480;
+  private final Random random = new Random();
+  private SpriteBatch batch;
+  private Sprite coin;
+  private List<Sprite> coins;
+  private long time = -1;
+  private float timeDelta = -1.0f;
+
+  @Override
+  public void create()
+  {
+    batch = new SpriteBatch();
+    Texture coinTexture = new Texture("coin32.png");
+    coins = new ArrayList<Sprite>();
+    for (int i = 0; i < COIN_NUMBER; i++)
+    {
+      coin = new Sprite(coinTexture);
+      coin.setY(-coinTexture.getHeight());
+      coin.setX(genRandomCoinXPos());
+      coins.add(coin);
+    }
+  }
+
+  @Override
+  public void render()
+  {
+    timeDelta = (System.currentTimeMillis() - time) / 1000.0f;
+
+    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    batch.begin();
+
+    if (time != -1)
+    {
+      for (int i = 0; i < COIN_NUMBER; i++)
+      {
+        if (coin.getY() > SCREEN_HEIGHT)
+        {
+          coin.setY(-32);
+        }
+
+        coin = coins.get(i);
+        coin.setY(coin.getY() + COIN_SPEED * timeDelta);
+        coin.draw(batch);
+      }
+    }
+
+    batch.end();
+
+    time = System.currentTimeMillis();
+  }
+
+  private int genRandomCoinXPos()
+  {
+
+    return random.nextInt(SCREEN_WIDTH);
+  }
 }
--- desktop/src/com/crazysaem/bts/desktop/DesktopLauncher.java
+++ desktop/src/com/crazysaem/bts/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.crazysaem.bts.BTSGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 480;
+        config.height = 800;
 		new LwjglApplication(new BTSGame(), config);
 	}
 }
